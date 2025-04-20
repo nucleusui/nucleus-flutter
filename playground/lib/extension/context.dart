c5a8d00c29ad8685ebc59787785573b0d5758e5f
Nazar <xpnazar@gmail.com>
2025-04-20 21:04:28 +0700

    chore: remove unused context extension

--- playground/lib/extension/context.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension AppContext on BuildContext {
-  /// Returns the current theme of the app.
-  ThemeData get theme => Theme.of(this);
-
-  /// Returns the current color scheme of the app.
-  ColorScheme get color => theme.colorScheme;
-}
